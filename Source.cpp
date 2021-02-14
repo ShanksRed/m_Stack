9d44c2a7bf5717b8c744aaf09bcd71cc3095b35e
lokengarviel16@gmail.com <lokengarviel16@gmail.com>
2021-02-14 12:31:27 +0300

    first look

---
+++ Source.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <string_view>
+#include <vector>
+#include <optional>
+#include"Stack.h"
+
+Stack<int>::type_t& enter_number(int condition);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Stack<int> m_stck{};
+    m_stck.print_stack();
+
+    int menu{-1};
+    constexpr std::string_view str_menu[]{
+        "0-Выход",
+        "1-Просмотреть стек",
+        "2-Значение верхнего элемента",
+        "3-Добавить элемент",
+        "4-Найти элемент",
+        "5-Удалить текеущий элемент",
+        "6-Случайное заполнение",
+        "7-Считать из файла",
+        "8-Запись в файл",
+        "9-Очистить стек",
+        "10-Вывод меню"
+    };
+ 
+    auto print_menu = [&]() constexpr {
+        for (auto&& element : str_menu) {
+            std::cout << element << std::endl;
+
+        }
+    };
+    
+    print_menu();
+
+    while (menu) {
+        std::cout << "Введите состояние: "; std::cin >> menu; 
+        switch (menu)
+        {
+
+        case 1:
+            m_stck.print_stack();
+        break;
+        case 2:
+        {
+            auto temp = m_stck.top();
+            if (!temp) std::cout << "Стек пуст" << std::endl;
+        }
+        break;
+        case 3:
+        {
+            try {
+                auto number = enter_number(3);
+                m_stck.push(number);
+            }
+            catch (const std::exception& e) {
+                std::cout << "Ошибка ввода" << std::endl;
+            }
+        }
+        break;
+        case 4:
+        {
+            try {
+                auto number = enter_number(4);
+                std::cout <<"Позиция числа в стеке: " << m_stck.findValue(number);
+               // temp==-1? std::cout << "Число не найдено" : std::cout << temp<<std::endl;
+            }
+            catch (const std::exception& e) {
+               // std::cout << "Ошибка ввода"<<std::endl;
+                std::cout <<e.what() << std::endl;
+            }
+        }
+        break;
+        case 5:
+        {
+            try {
+                m_stck.pop();
+            }
+            catch (const std::exception& e) {
+                std::cout << e.what() << std::endl;
+            }
+        }
+        break;
+        case 6:
+        {
+            int ld{}, rd{}, count{};
+            std::cout << "Введите диапазон и количество чисел :";
+            std::cin >> ld >> rd >> count;
+            m_stck.fill_rnumb(count, ld, rd);
+        }
+        break;
+        case 7:
+        {
+            m_stck.read_fr_file("m_txt.txt");           
+        }
+        break;
+        case 8:
+        {
+            m_stck.write_to_file("m_txt.txt");
+        }
+        break;
+        case 9:
+        {
+            m_stck.clear();
+        }
+        break;
+        case 10:
+        {
+            print_menu();
+        }
+        break;
+
+        case 0:
+            break;
+
+        default:
+            std::cout << "Ошибка"<<std::endl;
+        }
+
+    }
+   
+}
+
+template<typename T>
+inline void Stack<T>::print_stack() const noexcept
+{
+    /*
+    pointNode* head = this->head.get();
+    if (!head) std::cout << "стек пуст";
+    else
+        while (head) {
+            std::cout << head->data << " ";
+            head = head->next.get();
+
+        }
+        std::cout << std::endl;
+    */
+    
+    Stack<T>::Iterator iterator = this->Begin();
+
+
+
+}
+
+Stack<int>::type_t& enter_number (int condition) {
+    condition==4? std::cout << "Введите элемент для нахождения:": std::cout << "Введите число:";
+    Stack<int>::type_t number;
+    std::cin >> number;
+    if (std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        //std::cout << "Ошибка ввода " << std::endl;
+        throw std::exception("Ошибка ввода ");
+    }
+    else {
+        return number;
+    }
+}
---
+++ Stack.h
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <memory>
+#include <random>
+#include <filesystem>
+#include <fstream>
+
+
+template <typename T>
+class Stack {
+
+private:
+	struct pointNode {
+		T data;
+		std::unique_ptr<pointNode> next;
+
+		pointNode():data(std::move(T{})),next(std::move(std::unique_ptr<pointNode>{})){}
+		pointNode(T n_data, std::unique_ptr<pointNode> ptr) : data(std::move(n_data)), next(std::move(ptr)) {}
+
+		bool operator!=(Stack<T>::pointNode obj1) const {
+			if (this) return true;
+			else return false;
+		}
+		pointNode* operator++() {
+			return this->next.get();
+			//return m_temp;
+
+		}
+
+		const T& getValue() {
+			return this->data;
+		}
+
+	
+	};
+	std::unique_ptr<pointNode> head;
+public:
+	
+	class Iterator {
+		using Iterator_type = Stack<T>::pointNode;
+	public:
+		Iterator(Iterator_type* rNode) {
+			current_node = rNode;
+		}
+
+		//Iterator_type* operator=(Iterator_type* rNode) {
+		//	this->current_node = rNode;
+		//}
+
+
+	private:
+
+		Iterator_type* current_node;
+
+	};
+
+
+
+
+
+
+	using type_t = T;
+	Stack() : head(std::unique_ptr<pointNode>{}) {}
+	Stack(const T& data);
+	Stack(const T& data, std::unique_ptr<pointNode> ptr);
+	template<typename...Args>
+	Stack(Args&&...args);
+	void push(const T& data);
+	void pop() ;
+	const T& top() const  noexcept;
+	bool is_empty() const  noexcept;
+	inline void print_stack() const noexcept;
+	inline typename pointNode* Begin() noexcept;
+	inline typename pointNode* Next(pointNode*)noexcept;
+	inline typename Stack<T>::pointNode* End()noexcept;
+	T& findValue(T);
+	void clear() noexcept;
+	void fill_rnumb(int,int,int);
+	void write_to_file(std::filesystem::path filePath);
+	void read_fr_file(std::filesystem::path filePath);
+
+
+
+};
+
+template <typename T>
+Stack<T>::Stack(const T& data, std::unique_ptr<pointNode> ptr) :head(std::move(ptr)) {
+	head->data = std::move(data);
+}
+
+template <typename T>
+Stack<T>::Stack(const T& data) : head(std::make_unique<pointNode>(std::move(data), 
+								 std::move(std::unique_ptr<pointNode>{}))) {}
+
+
+template<typename T>
+template<typename ...Args>
+inline Stack<T>::Stack(Args && ...args)
+{
+	//int tmp{};
+	//(tmp += ... += (head = std::make_unique<pointNode>(std::forward<Args>(args), std::move(head)), 0));
+	((head = std::make_unique<pointNode>(std::forward<Args>(args),std::move(head))),...);
+	
+
+}
+
+template <typename T>
+void Stack<T>::push(const T& data) {
+	try {
+		auto temp = std::make_unique<pointNode>(data, nullptr);
+		if (head == nullptr) head = std::move(temp);
+		else {
+			temp->next = std::move(head);
+			head = std::move(temp);
+		}
+	}
+	catch (std::bad_alloc e)
+	{
+		std::cout << e.what() << std::endl;
+		return;
+	}
+
+}
+
+template <typename T>
+bool Stack<T>::is_empty() const noexcept {
+	if (head) return false;
+	else return true;
+}
+
+
+
+template<typename T>
+inline typename Stack<T>::pointNode* Stack<T>::Begin() noexcept
+{
+	pointNode* head = this->head.get();
+	return head;
+}
+
+template<typename T>
+inline typename Stack<T>::pointNode* Stack<T>::Next(pointNode* current) noexcept
+{
+	if (current) {
+		current= current->next.get();
+	}
+
+	else return nullptr;
+}
+
+template<typename T>
+inline typename Stack<T>::pointNode* Stack<T>::End() noexcept
+{
+	return nullptr;
+}
+
+template<typename T>
+T& Stack<T>::findValue(T search_numb)
+{
+	pointNode* head = this->head.get();
+	int count{};
+	while (head) {
+		if (head->data == search_numb)
+			return count;
+		else {
+			count++;
+			head = head->next.get();
+		}
+	}
+	throw std::exception("Число не найдено");
+}
+
+template <typename T>
+void Stack<T>::pop()  {
+	if (!is_empty())
+		head = std::move(head->next);
+	else
+		throw std::exception("Стек пуст");
+}
+
+template<typename T>
+inline void Stack<T>::clear() noexcept
+{
+	while (head) {
+		this->pop();
+	}
+}
+
+template<typename T>
+void Stack<T>::fill_rnumb(int counter, int l_border, int r_border)
+{
+	std::random_device rd;    
+	std::mt19937 rng(rd());    
+	std::uniform_int_distribution<int> uni(l_border, r_border);
+	while (counter) {
+		auto random_integer = uni(rng);
+		this->push(random_integer);
+		counter--;
+	}
+}
+
+template<typename T>
+inline void Stack<T>::write_to_file(std::filesystem::path filePath)
+{
+	std::wofstream writeFile(filePath, std::ios::trunc);
+	pointNode* head = this->head.get();
+	while (head) {
+		writeFile << head->data << " ";
+		head = head->next.get();
+		
+	}
+	writeFile.close();
+}
+
+template<typename T>
+inline void Stack<T>::read_fr_file(std::filesystem::path filePath)
+{
+	
+	std::wifstream readFile(filePath, std::ios::end);
+	T symb;
+	while (readFile >> symb) {
+		this->push(symb);
+	}
+	
+
+}
+
+template <typename T>
+const T& Stack<T>::top() const  noexcept {
+	if (!is_empty())
+		return head->data;
+
+	else return T{};
+
+
+}
+
+
